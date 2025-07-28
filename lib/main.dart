d76a465f5af14644f00b4a55b133c89efd4b0ac4
Harithya Wisesa <harithya77@gmail.com>
2025-07-28 12:43:07 +0700

    feat: install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:alquran/shared/theme.dart';
 import 'package:alquran/ui/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
